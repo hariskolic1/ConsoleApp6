57b4f07acab1d4b9101432a48d5be03e3d52f0b6
PC <PC@DESKTOP-ARTMFB0>
2021-08-23 18:11:03 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int brrr = int.Parse(Console.ReadLine());
+            int stp = int.Parse(Console.ReadLine());
+            int rez = Stepenovanje(brrr, stp);
+            Console.WriteLine(rez);
+
+        }
+               static int Stepenovanje(int broj, int stepen)
+        {
+            int br = broj;
+            for (; stepen > 1; stepen--)
+            {
+                broj *= br;
+
+            }
+            return broj;
+
+
+
+
+           /* int br = broj;
+            while(stepen > 1)
+            {
+                stepen--;
+                //broj = broj *br;
+                broj *= br;
+
+            }
+            return broj;*/
+
+        }
+     
+      
+    }
+}
